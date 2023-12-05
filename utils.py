b820ca70110df9c035ca912b3f853ff3c5a820bf
Yeonwoo Jang <yjang385@gmail.com>
2023-12-04 22:10:11 -0500

    reflect removed files

--- utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def hash(fpath):
-  with open(fpath, "rb") as f:
-    fhash = hashlib.md5()
-    while chunk := f.read(8192):
-      fhash.update(chunk)
-
-  return fhash.hexdigest()
